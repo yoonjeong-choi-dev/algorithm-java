02adbcaacefa33585af488244fdaa46d2756c5a9
yoonjeong-choi-dev <yjchoi7166@gmail.com>
2022-08-28 11:04:30 +0900

    [TIL] Daily Challenge Problem

---
+++ leet-code/src/from1301to1400/Prob1329SortTheMatrixDiagonally.java
@@ -0,0 +1,39 @@
+package from1301to1400;
+
+import java.util.PriorityQueue;
+
+// https://leetcode.com/problems/sort-the-matrix-diagonally/
+public class Prob1329SortTheMatrixDiagonally {
+    public int[][] diagonalSort(int[][] mat) {
+        int rowSize = mat.length, colSize = mat[0].length;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        int curRange;
+
+        // bottom-left
+        for (int row = 0; row < rowSize; row++) {
+            curRange = Math.min(rowSize - row, colSize);
+            for (int i = 0; i < curRange; i++) {
+                minHeap.add(mat[row + i][i]);
+            }
+
+            for (int i = 0; i < curRange; i++) {
+                mat[row + i][i] = minHeap.poll();
+            }
+        }
+
+        // top-right
+        for (int col = 1; col < colSize; col++) {
+            curRange = Math.min(rowSize, colSize - col);
+            for (int i = 0; i < curRange; i++) {
+                minHeap.add(mat[i][col + i]);
+            }
+
+            for (int i = 0; i < curRange; i++) {
+                mat[i][col + i] = minHeap.poll();
+            }
+        }
+
+        return mat;
+    }
+}
