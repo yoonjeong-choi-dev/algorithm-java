80716f2df1c85c39e1a59c6b73f27bca0fe2fd49
yoonjeong-choi-dev <yjchoi7166@gmail.com>
2022-09-04 01:14:22 +0900

    [TIL] Study Plan - Programming Skills 3
    - Prob 525
    - Prob 1798

---
+++ leet-code/src/from501to600/Prob525ContiguousArray.java
@@ -0,0 +1,29 @@
+package from501to600;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/contiguous-array/
+// Ref : Problem 325
+public class Prob525ContiguousArray {
+    public int findMaxLength(int[] nums) {
+        // sum of( nums[:i] ) -> i
+        // 이때 0은 -1로 잡아준다
+        Map<Integer, Integer> prefixIndex = new HashMap<>();
+
+        int ans = 0;
+        int curSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curSum += nums[i] == 0 ? -1 : 1;
+
+            if (curSum == 0) {
+                ans = i + 1;
+            } else {
+                ans = Math.max(ans, i - prefixIndex.getOrDefault(curSum, i));
+            }
+
+            if (!prefixIndex.containsKey(curSum)) prefixIndex.put(curSum, i);
+        }
+        return ans;
+    }
+}
