23876b2ed80e87da0f02f44c3cd6e0f5c58464d7
yoonjeong-choi-dev <yjchoi7166@gmail.com>
2022-09-08 00:20:29 +0900

    [TIL] Top Microsoft Questions
    - Prob 1578
    - Prob 1615
    - Prob 1653

---
+++ leet-code/src/from1601to1700/Prob1653MinimumDeletionsToMakeStringBalanced.java
@@ -0,0 +1,36 @@
+package from1601to1700;
+
+// https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/
+public class Prob1653MinimumDeletionsToMakeStringBalanced {
+    public int minimumDeletions(String s) {
+        int len = s.length();
+
+        if (len == 1) return 0;
+
+        // leftB[i] : 0,...,(i-1) 중에 B 의 개수
+        int[] leftB = new int[len];
+        for (int i = 1; i < len; i++) {
+            leftB[i] = leftB[i - 1];
+            if (s.charAt(i - 1) == 'b') {
+                leftB[i] += 1;
+            }
+        }
+
+        // rightA[i] : i+1,..,len-1 중에 A의 개수
+        int[] rightA = new int[len];
+        for (int i = len - 2; i >= 0; i--) {
+            rightA[i] = rightA[i + 1];
+            if (s.charAt(i + 1) == 'a') {
+                rightA[i] += 1;
+            }
+        }
+
+        int ans = len;
+        for (int i = 0; i < len; i++) {
+            // s[i] == A : 마지막 A 라고 가정 => 왼쪽 B 및 오른쪽 A 삭제
+            // s[i] == B : 첫번째 B 라고 가정 => 왼쪽 B 및 오른쪽 A 삭제
+            ans = Math.min(ans, leftB[i] + rightA[i]);
+        }
+        return ans;
+    }
+}
