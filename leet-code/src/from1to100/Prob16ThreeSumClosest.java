8796c2c189998f7c6529adc440edb214f7208e66
yoonjeong-choi-dev <yjchoi7166@gmail.com>
2022-09-26 23:06:15 +0900

    [TIL] Study Plan - LeetCode75 2
    - Prob 16

---
+++ leet-code/src/from1to100/Prob16ThreeSumClosest.java
@@ -0,0 +1,33 @@
+package from1to100;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/3sum-closest/
+public class Prob16ThreeSumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int bestDiff = Integer.MAX_VALUE;
+        int len = nums.length;
+
+        int left, right, curDiff;
+        for (int i = 0; i < len; i++) {
+            left = i + 1;
+            right = len - 1;
+
+            while (left < right) {
+                curDiff = target - (nums[i] + nums[left] + nums[right]);
+                if (curDiff == 0) return target;
+
+                if (Math.abs(curDiff) < Math.abs(bestDiff)) {
+                    bestDiff = curDiff;
+                }
+
+                if (curDiff < 0) right--;
+                else left++;
+            }
+        }
+
+        return target - bestDiff;
+    }
+}
